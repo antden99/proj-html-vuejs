34b274b3a91e1bb40473f9942d390540afb22ebd
antden99 <antoniodenigris@icloud.com>
2024-04-08 11:49:33 +0200

    add the header and carousel structure

---
+++ src/components/AppCarousel.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name: "AppCarousel",
+}
+
+</script>
+
+<template>
+
+<div class="carosello">
+    Carosello
+</div>
+
+</template>
+
+<style scoped>
+.carosello{
+    width: 100%;
+    background-color: bisque;
+    height: 200px;
+}
+
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,50 @@
+<script>
+export default {
+  name: "AppHeader",
+}
+
+</script>
+
+<template>
+  <div class="item_1">
+    <div class="container">
+      News
+    </div>
+  </div>
+  <div class="item_2">
+    <div class="container">
+      Logo
+    </div>
+  </div>
+  <div class="item_3">
+    <div class="container">
+      NavBar
+    </div>
+  </div>
+
+
+</template>
+
+<style scoped>
+.container {
+  width: 80%;
+  max-width: 1200px;
+  margin: auto;
+  border: 2px dotted black;
+  height: 100%;
+}
+
+.item_1 , .item_3{
+  background-color: red;
+  color: white;
+  width: 100%;
+  height: 50px;
+}
+
+.item_2{
+  background-color: aqua;
+  width: 100%;
+  height: 100px;
+}
+
+</style>
